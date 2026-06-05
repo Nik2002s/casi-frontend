a0eed0fc0b09d6a851d14cd4273850224e4db717
Nikhil Sharma <nikhilsharma3535@gmail.com>
2026-06-05 15:34:37 +0530

    removed something

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -11,24 +11,24 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
-// const firebaseConfig = {
-//   apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-//   measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-// }
 const firebaseConfig = {
-  apiKey: "AIzaSyBOfK8qXUVNtOgnrBt15ZwhuMkdOtxy41o",
-  authDomain: "casi-64bdd-12277.firebaseapp.com",
-  projectId: "casi-64bdd-12277",
-  storageBucket: "casi-64bdd-12277.firebasestorage.app",
-  messagingSenderId: "353424150606",
-  appId: "1:353424150606:web:d09805adad99b7ccc5731f",
-  measurementId: "G-TB2RP7HEQ2"
-};
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+}
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBOfK8qXUVNtOgnrBt15ZwhuMkdOtxy41o",
+//   authDomain: "casi-64bdd-12277.firebaseapp.com",
+//   projectId: "casi-64bdd-12277",
+//   storageBucket: "casi-64bdd-12277.firebasestorage.app",
+//   messagingSenderId: "353424150606",
+//   appId: "1:353424150606:web:d09805adad99b7ccc5731f",
+//   measurementId: "G-TB2RP7HEQ2"
+// };
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
